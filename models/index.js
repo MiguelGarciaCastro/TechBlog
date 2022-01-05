b63aec5d1cbf3ff63021ae27f7c919b030323ed4
Miguel Garcia-Castro <garciamiguel7700@yahoo.com>
2022-01-05 16:39:29 -0500

    final

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
-const User = require('./userData');
-const blogPost = require('./blogPostsData')
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-
-
-blogPost.belongsTo(User, {
-    foreignKey: 'post_creator',
-    onDelete: "CASCADE"
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
 });
 
-User.hasMany(blogPost, {
-    foreignKey: 'post_creator',
-    onDelete: "CASCADE"
-})
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
 
-module.exports = {blogPost, User}
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+})
+module.exports = { User, Post, Comment };
--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-require('dotenv').config()
-const sequelize = require('./config/connection')
-const express = require('express')
-const exphbs = require('express-handlebars')
-const controllers = require('./controllers')
-
-
-const PORT = 3001
-const app = express()
-
-app.engine('handlebars', exphbs())
-app.set('view engine', 'handlebars')
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
-
-app.use(controllers)
-
-sequelize.sync({ force: true }).then(() => {
-    console.log(`Sequelize connected!`)
-
-})
-// html routes
-
-
-app.listen(PORT, () => console.log(`Go to http://localhost:${PORT}`))
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: { maxAge: 36000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
